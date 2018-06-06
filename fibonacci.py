1fc9d197035d9a66ae8fa765519c46c2acfc9f7f
LENOVO-PC\diana_000 <dianarachvak6@gmail.com>
2018-06-06 05:51:30 -0400

    different ways of solving fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -9,3 +9,24 @@ def F(n):
         return F(n-1)+F(n-2)
 
 print(F(5))
+
+#another version
+a,b = 0, 1
+max = 0
+for i in range(0, 10):
+	if max < a:
+		max = a
+	print (a, end=" ")
+	a, b = b, a + b
+
+print ("\n max # is: ", max)
+
+#fibonacci using generators
+def fib(num):
+	a, b = 0, 1
+	for i in range (0, num):
+		yield "{}: {}".format(i+1, a)
+		a, b = b, a + b
+
+for item in fib(10):
+	print (item)
